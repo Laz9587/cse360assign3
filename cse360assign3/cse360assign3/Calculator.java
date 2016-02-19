abc8ec1042af5ada5fc553a6e5b3fc321eb02cad
Lazaro Ortiz <Lazaro_9587@hotmail.com>
2016-02-19 16:44:40 -0700

    Initial check-in of assign3

---
+++ cse360assign3/cse360assign3/Calculator.java
@@ -0,0 +1,66 @@
+/**
+ * Class to perform basic calculator functions.
+ * 
+ * @author Lazaro Ortiz pin # 621
+ */
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * Initializes the calculator's total to zero
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * getTotal - provides the calculator's total value
+	 * @return the value of total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * add - adds the value in the parameter to the calculator total
+	 * @param value the value that will be added to the total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * subtract - subtracts the value in the parameter from the calculator total
+	 * @param value the value that will be subtracted from total
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * multiply - multiplies the value in the parameter to the total
+	 * @param value the value that the total will be multiplied by
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * divide - divides the total by the value in the parameter
+	 * @param value the value that total will be divided by
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * getHistory - provides the history of all the inputs it received to include operators and operands
+	 * @return string of the history created from the inputs the calculator received
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
